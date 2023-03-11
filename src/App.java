9489f2f738dde8a3846651013c5a9aa5a6badaf8
Team 0x6861636B <95329795+proplayer919@users.noreply.github.com>
2023-03-12 07:41:37 +1100

    update

---
+++ src/App.java
@@ -0,0 +1,16 @@
+package org.fifly.recolite;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
+
+class App {
+    public JFrame frame = new JFrame("Recolite");
+    public static void Main() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+    }
+}
